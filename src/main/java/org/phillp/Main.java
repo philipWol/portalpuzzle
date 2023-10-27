da8800ff4f61c6e7307c8f2bd63e63c5c13eac13
philipWol <132010196+philipWol@users.noreply.github.com>
2023-10-27 17:59:56 +0200

    completed task requirement

--- src/main/java/org/phillp/Main.java
+++ src/main/java/org/phillp/Main.java
@@ -1,9 +1,34 @@
 package org.phillp;
 
+import org.phillp.quantum.Point;
+
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter coordinates for Point A: ");
+        Point pointA = createPoint(scanner);
+        System.out.println("Enter coordinates for Point B: ");
+        Point pointB = createPoint(scanner);
+        if (pointA.calculateEnergy() == pointB.calculateEnergy()) {
+            System.out.println("A stable portal can be opened.");
+        } else {
+            System.out.println("A stable portal cannot be opened.");
         }
+
     }
+
+    public static Point createPoint(Scanner scanner) {
+
+        System.out.println("Enter x coordinate ");
+        int x = scanner.nextInt();
+        System.out.println("Enter y coordinate ");
+        int y = scanner.nextInt();
+        System.out.println("Enter z coordinate ");
+        int z = scanner.nextInt();
+        return new Point(x, y, z);
+    }
+}
